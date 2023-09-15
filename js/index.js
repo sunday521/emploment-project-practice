836e9e638ce174ff9ad39222e1a3a03eb3d9aa3a
sunday521 <3032317941@qq.com>
2023-09-15 11:42:38 +0800

    完成首页-渲染概览数据

--- js/index.js
+++ js/index.js
@@ -9,3 +9,34 @@ renderUsername();
 // 功能5：首页-退出登录
 // 先清除本地缓存中的用户数据，然后跳转到登录页
 logout();
+
+// 功能6：首页-获取统计数据（接口权限控制）
+// 这个接口比较特殊，要求请求头携带用户token才能成功调用
+// 如果用户token失效（过期或被篡改），需要重新登录
+async function getData() {
+  try {
+    const res = await axios({
+      url: "/dashboard",
+      headers: {
+        Authorization: localStorage.getItem("token"),
+      },
+    });
+    // console.log(res.data.data);
+    const { groupData, overview, provinceData, salaryData, year } =
+      res.data.data;
+    // 功能6-1：渲染概览数据
+    renderOverview(overview);
+  } catch (err) {
+    console.dir(err.response.data);
+  }
+}
+
+getData();
+
+function renderOverview(overview) {
+  // 观察到类名和保存值的变量全部一致，因此可以循环渲染
+  // console.log(overview);
+  Object.keys(overview).forEach((key) => {
+    document.querySelector(`.${key}`).innerHTML = overview[key];
+  });
+}
