120ebc703b67a725823979f9d47e4fc1aa803eba
sunday521 <3032317941@qq.com>
2023-09-15 10:13:02 +0800

    完成axios基地址配置

---
+++ js/common.js
@@ -0,0 +1,14 @@
+// 配置axios基地址
+axios.defaults.baseURL = "https://hmajax.itheima.net";
+
+// 测试是否正常使用
+// axios({
+//   url: "/register",
+//   method: "POST",
+//   data: {
+//     username: "evannotfound666",
+//     password: "123456",
+//   },
+// }).then((res) => {
+//   console.log(res.data);
+// });
