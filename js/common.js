38f87ba0a19cbacc241f848a14dccee79360da63
sunday521 <3032317941@qq.com>
2023-09-15 11:10:47 +0800

    完成首页-页面访问控制

--- js/common.js
+++ js/common.js
@@ -22,3 +22,15 @@ function showToast(msg) {
 
 // 测试是否正常使用
 // showToast("注册成功！");
+
+// 检测用户是否已登录
+function checkLogin() {
+  const token = localStorage.getItem("token");
+  // 如果用户没有登录，提示用户，然后跳转到登录页
+  if (!token) {
+    showToast("请先登录！");
+    setTimeout(() => {
+      location.href = "./login.html";
+    }, 1500);
+  }
+}
